3788f85085da19e4c77984ab24ee6d049e619e5b
Chris Villa <chrisvilla@me.com>
2016-02-06 19:34:32 +0000

    Ditch browserify in favour of webpack

--- src/main.jsx
+++ src/main.jsx
@@ -1,2 +1,1 @@
 var elemental = require('elemental');
-
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  loaders: [
+    {
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel', // 'babel-loader' is also a legal name to reference
+      query: {
+        presets: ['react', 'es2015']
+      }
+    }
+  ]
+}
