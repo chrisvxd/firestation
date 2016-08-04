bde44a5a511e23f7b3c00a8a440389fe9ced5877
Chris Villa <chrisvilla@me.com>
2016-08-04 18:15:33 +0100

    Don't render app unless auth method finished

--- src/entry.js
+++ src/entry.js
@@ -4,8 +4,6 @@ import App from './app.jsx';
 
 import configuration from '../firestation.config.js';
 
-ReactDOM.render(<App />, document.getElementById('app'));
-
 configuration.auth(function (ref) {
     ReactDOM.render(<App />, document.getElementById('app'));
 });
