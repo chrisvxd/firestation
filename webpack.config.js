9bb556e95a0366ff76a713affc0bbd53f43ee7c1
Chris Villa <chrisvilla@me.com>
2016-08-05 20:42:13 +0100

    Undo strange webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,15 @@
 var path = require('path');
 var webpack = require('webpack');
- 
+
+var env = (process.env.NODE_ENV || 'dev')
+
 module.exports = {
   entry: './src/entry.js',
   output: { path: __dirname, filename: 'dist/bundle.js' },
   resolve: {
     alias: {
-      'components': path.join(__dirname, 'src/components')
+      'components': path.join(__dirname, 'src/components'),
+      'config': path.join(__dirname, 'config', env)
     }
   },
   module: {
@@ -23,7 +26,7 @@ module.exports = {
         test: /\.less$/,
         loader: "style!css!less"
       },
-      { 
+      {
         test: /\.css$/,
         loader: "style-loader!css-loader"
       },
