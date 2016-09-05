e831cdb19799734aaad77922cb6725d4b53db726
Chris Villa <chrisvilla@me.com>
2016-09-05 23:20:41 +0100

    Major refactor to enable background refs
    Involved moving many methods from firebase-table.jsx to app.jsx hence huge diff.

--- src/app.jsx
+++ src/app.jsx
@@ -13,18 +13,34 @@ var Card = elemental.Card;
 var Form = elemental.Form;
 var FormField = elemental.FormField;
 var FormInput = elemental.FormInput;
+var Spinner = elemental.Spinner;
 
 import FirebaseTable from 'components/firebase-table.jsx';
 
+import {getNestedValue, setNestedValue} from './utils.js';
+
+var defaultResolve = function (key, val, callback) {
+    callback(val);
+};
+
 export default React.createClass({
+    componentWillMount: function () {
+        this.items = {};
+        this.layouts = {};
+        this.handlers = {};
+        this.refs = {};
+        this.refSelected(0);
+    },
     getInitialState: function () {
         document.title = configuration.title + ' | ' + configuration.refs[0].title;
         return {
             currentRefIndex: 0,
             currentItems: [],
+            currentHandlers: {},
             rangeStart: configuration.refs[0].rangeStart || 1,
             rangeEnd: configuration.refs[0].rangeEnd || 10,
-            filteredSize: 0
+            filteredSize: 0,
+            loaded: false
         };
     },
     refSelected: function (refIndex) {
@@ -35,6 +51,136 @@ export default React.createClass({
             rangeStart: configuration.refs[refIndex].rangeStart || 1,
             rangeEnd: configuration.refs[refIndex].rangeEnd || 10
         });
+
+        if (this.items[refIndex] === undefined) {
+            this.setState({loaded: false});
+            this.layouts[refIndex] = {filters: {}};
+            this.makeQuery(refIndex);
+        } else {
+            this.prepareAndSetState(refIndex);
+        }
+    },
+    makeQuery: function (refIndex) {
+        this.items[refIndex] = [];
+
+        const config = configuration.refs[refIndex];
+
+        const ref = (config.getRef || function () {return config.ref})();
+        const batchRef = config.batchRef || ref;
+
+        var _this = this;
+
+        var monitorRef = function () {
+            ref.on('child_added', function (snapshot) {this.processSnapshot(refIndex, snapshot)}.bind(this)),
+            batchRef.on('child_changed', function (snapshot) {this.processSnapshot(refIndex, snapshot)}.bind(this)),
+            batchRef.on('child_removed', function (snapshot) {this.removeSnapshot(refIndex, snapshot)}.bind(this))
+        };
+
+        // Run value once, then watch for children added
+        batchRef.once('value', function (snapshot) {
+            var i = 0;
+
+            console.log('Queried firebase!');
+
+            var resolve = configuration.refs[refIndex].resolve || defaultResolve;
+
+            snapshot.forEach(function (snapshotChild) {
+                resolve(snapshotChild.key(), snapshotChild.val(), function (val) {
+                    var key = snapshotChild.key();
+
+                    val._key = key;
+
+                    this.items[refIndex].push({
+                        val: val,
+                        key: key
+                    });
+
+                    i += 1;
+
+                    if (i === snapshot.numChildren()) {
+                        this.prepareAndSetState(refIndex);
+                        monitorRef.bind(this)();
+                    };
+                }.bind(this));
+            }.bind(this));
+        }.bind(this));
+
+    },
+    processSnapshot: function (refIndex, snapshot) {
+        var resolve = configuration.refs[refIndex].resolve || defaultResolve;
+
+        resolve(snapshot.key(), snapshot.val(), function (val) {
+            var key = snapshot.key();
+
+            var existingIndex = _.findIndex(this.items[refIndex], {'key': key});
+
+            val._key = key;
+
+            var newItem = {
+                val: val,
+                key: key
+            };
+
+            if (existingIndex > -1) {
+                if (JSON.stringify(this.items[refIndex][existingIndex]) !== JSON.stringify(newItem)) {
+                    this.items[refIndex][existingIndex] = newItem
+                    this.setStateIfRefActive(refIndex);
+                }
+            } else {
+                this.items[refIndex].push(newItem);
+                this.setStateIfRefActive(refIndex);
+            };
+        }.bind(this));
+    },
+    removeSnapshot: function (refIndex, snapshot) {
+        var key = snapshot.key();
+        var existingIndex = _.findIndex(this.items[refIndex], {'key': key});
+
+        this.items[refIndex].splice(existingIndex, 1);
+
+        this.setStateIfRefActive(refIndex);
+    },
+    setStateIfRefActive: function (refIndex) {
+        if (refIndex === this.state.currentRefIndex) {
+            this.prepareAndSetState(refIndex)
+        }
+    },
+    prepareAndSetState: function (refIndex) {
+        this.sortItems(refIndex); // in place
+        this.setState({
+            currentItems: this.filterItems(refIndex),
+            filteredSize: this.items[refIndex].length,
+            loaded: true
+        });
+    },
+    // Filters this.items and returns filtered
+    filterItems: function (refIndex) {
+        var items = this.items[refIndex];
+
+        for (var k in this.layouts[refIndex].filters) {
+            items = _.filter(items, this.layouts[refIndex].filters[k]);
+        }
+
+        return items;
+    },
+    // Sorts in place
+    sortItems: function (refIndex) {
+        if (this.layouts[refIndex].orderBy) {
+            this.items[refIndex] = _.orderBy(this.items[refIndex], function (item) {
+                return getNestedValue(item.val, this.layouts[refIndex].orderBy);
+            }.bind(this), this.layouts[refIndex].orderByDirection);
+        };
+    },
+    setCurrentOrderBy: function (orderBy, direction) {
+        this.layouts[this.state.currentRefIndex].orderBy = orderBy
+        this.layouts[this.state.currentRefIndex].orderByDirection = direction
+        this.prepareAndSetState(this.state.currentRefIndex);
+    },
+    setFiltersByKey: function (filtersByKey) {
+        this.layouts[this.state.currentRefIndex].filtersByKey = filtersByKey
+    },
+    setOpenFilterFields: function (openFilterFields) {
+        this.layouts[this.state.currentRefIndex].openFilterFields = openFilterFields
     },
     itemsLoaded: function (items) {
         this.setState({
@@ -46,6 +192,43 @@ export default React.createClass({
         this.setState({
             filteredSize: items.length
         });
+    },
+    setCurrentHandlers: function (handlers) {
+        this.setState({
+            currentHandlers: handlers
+        });
+    },
+    registerFilterAndRun: function (key, value) {
+        if (value) {
+            this.layouts[this.state.currentRefIndex].filters[key] = function(val){
+                return function (o) {
+                    var queriedValue = getNestedValue(o.val, key);
+
+                    if (queriedValue.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) {
+                        return true
+                    } else if (queriedValue.toString().score(val) > 0.4) {
+                        return true
+                    } else {
+                        return false
+                    }
+                }
+            }(value);
+
+        } else {
+            delete this.layouts[this.state.currentRefIndex].filters[key];
+        };
+
+        var items = this.filterItems(this.state.currentRefIndex);
+
+        this.layouts[this.state.currentRefIndex].items = items;
+
+        this.setState({
+            currentItems: items,
+            filteredSize: items.length
+        });
+    },
+    updateBackgroundItems: function (items) {
+
     },
     handleRangeStartChange: function (event) {
         var rangeDiff = this.state.rangeEnd - this.state.rangeStart;
@@ -93,6 +276,32 @@ export default React.createClass({
             }
         };
 
+        var table;
+
+        if (!this.state.loaded) {
+            table = (<div style={{width: "100%", height: "800px", display: "flex", justifyContent: "center", alignItems: "center"}}>
+                 <Spinner size="lg" />
+             </div>)
+        } else {
+            table = <FirebaseTable
+                key={this.state.currentRefIndex}
+                refIndex={this.state.currentRefIndex}
+                rangeStart={this.state.rangeStart}
+                rangeEnd={this.state.rangeEnd}
+                items={this.state.currentItems}
+                itemsLoaded={this.itemsLoaded}
+                itemsFiltered={this.itemsFiltered}
+                setCurrentOrderBy={this.setCurrentOrderBy}
+                setOpenFilterFields={this.setOpenFilterFields}
+                setFiltersByKey={this.setFiltersByKey}
+                orderBy={this.layouts[this.state.currentRefIndex].orderBy}
+                orderByDirection={this.layouts[this.state.currentRefIndex].orderByDirection}
+                openFilterFields={this.layouts[this.state.currentRefIndex].openFilterFields}
+                filtersByKey={this.layouts[this.state.currentRefIndex].filtersByKey}
+                registerFilterAndRun={this.registerFilterAndRun}
+            />
+        }
+
         return (
             <div>
                 <div className="Nav">
@@ -116,14 +325,7 @@ export default React.createClass({
                     </Form>
                 </div>
 
-                <FirebaseTable
-                    key={this.state.currentRefIndex}
-                    refIndex={this.state.currentRefIndex}
-                    rangeStart={this.state.rangeStart}
-                    rangeEnd={this.state.rangeEnd}
-                    itemsLoaded={this.itemsLoaded}
-                    itemsFiltered={this.itemsFiltered}
-                />
+                {table}
             </div>
         )
     }
--- src/components/firebase-table.jsx
+++ src/components/firebase-table.jsx
@@ -19,117 +19,22 @@ import configuration from '../../firestation.config.js';
 import Row from './row.jsx';
 import {getNestedValue, setNestedValue} from '../utils.js';
 
-var defaultResolve = function (key, val, callback) {
-    callback(val);
-};
-
 export default React.createClass({
     getInitialState: function () {
         return {
             items: [],
-            orderBy: configuration.refs[this.props.refIndex].orderBy,
-            orderByDirection: configuration.refs[this.props.refIndex].orderByDirection,
-            openFilterFields: [],
-            filtersByKey: {},
-            loaded: false
+            orderBy: (this.props.orderBy || configuration.refs[this.props.refIndex].orderBy),
+            orderByDirection: (this.props.orderByDirection || configuration.refs[this.props.refIndex].orderByDirection),
+            openFilterFields: this.props.openFilterFields || [],
+            filtersByKey: this.props.filtersByKey || {},
+            loaded: true
         };
     },
     componentWillMount: function() {
-        this.currentOrderBy = configuration.refs[this.props.refIndex].orderBy;
-        this.currentOrderByDirection = configuration.refs[this.props.refIndex].orderByDirection;
-        this.filters = {};
+        const config = configuration.refs[this.props.refIndex];
 
-        this.makeQuery();
-    },
-    makeQuery: function () {
-        this.items = [];
-
-        if (this.ref !== undefined) {
-            this.ref.off();
-        };
-
-        this.ref = configuration.refs[this.props.refIndex].ref;
-        this.batchRef = configuration.refs[this.props.refIndex].batchRef || this.ref;
-
-        var monitorRef = function () {
-            this.ref.on('child_added', this.processSnapshot);
-            this.batchRef.on('child_changed', this.processSnapshot);
-            this.batchRef.on('child_removed', this.removeSnapshot);
-        };
-
-        // Run value once, then watch for children added
-        this.batchRef.once('value', function (snapshot) {
-            var i = 0;
-
-            console.log('Queried firebase!');
-
-            var resolve = configuration.refs[this.props.refIndex].resolve || defaultResolve;
-
-            snapshot.forEach(function (snapshotChild) {
-                resolve(snapshotChild.key(), snapshotChild.val(), function (val) {
-                    var key = snapshotChild.key();
-
-                    val._key = key;
-
-                    this.items.push({
-                        val: val,
-                        key: key
-                    });
-
-                    i += 1;
-
-                    if (i === snapshot.numChildren()) {
-                        this.prepareAndSetState({items: this.items, loaded: true});
-                        monitorRef.bind(this)();
-                    };
-                }.bind(this));
-            }.bind(this));
-        }.bind(this));
-
-    },
-    prepareAndSetState: function (state) {
-        this.sortItems(); // in place
-        state.items = this.filterItems(); // not in place
-        this.setState(state);
-        this.props.itemsLoaded(this.state.items);
-    },
-    processSnapshot: function (snapshot) {
-        var resolve = configuration.refs[this.props.refIndex].resolve || defaultResolve;
-
-        resolve(snapshot.key(), snapshot.val(), function (val) {
-            var key = snapshot.key();
-
-            var existingIndex = _.findIndex(this.items, {'key': key});
-
-            val._key = key;
-
-            var newItem = {
-                val: val,
-                key: key
-            };
-
-            if (existingIndex > -1) {
-                if (JSON.stringify(this.items[existingIndex]) !== JSON.stringify(newItem)) {
-                    this.items[existingIndex] = newItem
-                    this.prepareAndSetState({items: this.items});
-                }
-            } else {
-                this.items.push(newItem);
-                this.prepareAndSetState({items: this.items});
-            };
-        }.bind(this));
-    },
-    removeSnapshot: function (snapshot) {
-        var key = snapshot.key();
-        var existingIndex = _.findIndex(this.items, {'key': key});
-
-        this.items.splice(existingIndex, 1);
-        this.setState({
-            items: this.items
-        });
-    },
-    componentWillUnmount: function () {
-        configuration.refs[this.props.refIndex].ref.off();
+        this.currentOrderBy = (this.props.orderBy || configuration.refs[this.props.refIndex].orderBy);
+        this.currentOrderByDirection = (this.props.orderByDirection || configuration.refs[this.props.refIndex].orderByDirection);
     },
     handleFilterChange: function (key, title, event) {
         const value = event.target.value.replace(title + ': ', '').replace(title + ':', '');
@@ -149,37 +54,11 @@ export default React.createClass({
             filtersByKey: filtersByKey
         });
 
-        this.filterDebounced = _.debounce(this.registerFilterAndRun, 250, {maxWait: 1000});
+        this.filterDebounced = _.debounce(this.props.registerFilterAndRun, 250, {maxWait: 1000});
 
         this.filterDebounced(key, value);
-    },
-    registerFilterAndRun: function (key, value) {
-        if (value) {
-            this.filters[key] = function(val){
-                return function (o) {
-                    var queriedValue = getNestedValue(o.val, key);
 
-                    if (queriedValue.toString().toLowerCase().indexOf(val.toLowerCase()) > -1) {
-                        return true
-                    } else if (queriedValue.toString().score(val) > 0.4) {
-                        return true
-                    } else {
-                        return false
-                    }
-                }
-            }(value);
-
-        } else {
-            delete this.filters[key];
-        };
-
-        var items = this.filterItems();
-
-        this.setState({
-            items: items,
-        });
-
-        this.props.itemsFiltered(items);
+        this.props.setFiltersByKey(filtersByKey);
     },
     handleSortClick: function (key) {
         if (key === this.currentOrderBy) {
@@ -193,12 +72,12 @@ export default React.createClass({
             this.currentOrderByDirection = 'asc';
         };
 
-        // this.makeQuery();
-
-        this.prepareAndSetState({
+        this.setState({
             orderBy: this.currentOrderBy,
             orderByDirection: this.currentOrderByDirection
         });
+
+        this.props.setCurrentOrderBy(this.currentOrderBy, this.currentOrderByDirection)
     },
     handleSearchClick: function (key, event) {
         var openFilterFields = this.state.openFilterFields;
@@ -211,6 +90,8 @@ export default React.createClass({
         this.setState({
             openFilterFields: openFilterFields
         });
+
+        this.props.setOpenFilterFields(openFilterFields);
     },
     handleSearchSubmit: function (key, event) {
         event.preventDefault();
@@ -221,24 +102,6 @@ export default React.createClass({
         this.setState({
             openFilterFields: openFilterFields
         });
-    },
-    // Filters this.items and returns filtered
-    filterItems: function () {
-        var items = this.items;
-
-        for (var k in this.filters) {
-            items = _.filter(items, this.filters[k]);
-        }
-
-        return items;
-    },
-    // Sorts in place
-    sortItems: function () {
-        if (this.currentOrderBy) {
-            this.items = _.orderBy(this.items, function (item) {
-                return getNestedValue(item.val, this.currentOrderBy);
-            }.bind(this), this.currentOrderByDirection);
-        };
     },
     // Should probably be another component
     renderHeaders: function () {
@@ -268,7 +131,7 @@ export default React.createClass({
             var searchGlyph = null;
 
             if (config.canFilter !== false) {
-                if (this.filters[key] !== undefined) {
+                if (this.state.filtersByKey[key] !== undefined) {
                     searchGlyph = (<span className="HeaderSearchIcon" onClick={this.handleSearchClick.bind(this, key)}>
                         <Glyph icon="search" type="primary"></Glyph>
                     </span>)
@@ -313,14 +176,6 @@ export default React.createClass({
     },
     render: function () {
 
-        if (!this.state.loaded) {
-            return (
-                <div style={{width: "100%", height: "800px", display: "flex", justifyContent: "center", alignItems: "center"}}>
-                    <Spinner size="lg" />
-                </div>
-            )
-        }
-
         var headers = this.renderHeaders();
 
         // Dynamically create rows based on ref configuration
@@ -330,14 +185,14 @@ export default React.createClass({
         var rangeEnd = this.props.rangeEnd;
         var rangeDiff = rangeEnd - rangeStart;
 
-        if (this.state.items.length < rangeEnd) {
-            rangeEnd = this.state.items.length;
+        if (this.props.items.length < rangeEnd) {
+            rangeEnd = this.props.items.length;
         }
 
-        if (this.state.items.length >= rangeStart && rangeEnd >= rangeStart) {
+        if (this.props.items.length >= rangeStart && rangeEnd >= rangeStart) {
             for (var i = rangeStart - 1; i < rangeEnd; i++) {
-                var key = this.state.items[i].key;
-                var val = this.state.items[i].val;
+                var key = this.props.items[i].key;
+                var val = this.props.items[i].val;
                 rows.push(<Row key={key} item={val} itemKey={key} refIndex={this.props.refIndex} />);
             };
         }
